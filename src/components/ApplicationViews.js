0a6b618278f235696844d55c9e343b946a9fc941
Spencer Truett <srtruett1993@gmail.com>
2020-01-29 14:52:57 -0600

    created initial ApplicationView framework

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+
+export default () => {
+    return (
+        <>
+                  <Route exact path="/">
+
+                  </Route>
+
+                  <Route exact path="/news">
+
+                  </Route>
+
+                  <Route exact path="/events">
+
+                  </Route>
+
+                  <Route exact path="/tasks">
+
+                  </Route>
+
+
+        </>
+    )
+}
