57919419b1cb2d031751d3c8a5daa1f77afa6020
Spencer Truett <srtruett1993@gmail.com>
2020-01-30 10:48:18 -0600

    initial delete attempt, not working

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,8 +1,11 @@
-import React from "react"
+import React, { useContext } from "react"
+import { NewsContext } from "./NewsProvider"
 // import "./News.css"
 
-export default ({ news }) => {
+export default (props, { news }) => {
   const url = `https://www.${news.url}`
+  const { NewsArray, deleteNews } = useContext(NewsContext)
+
  return (
     <section className="news">
         <h3 className="news__name">
@@ -11,6 +14,17 @@ export default ({ news }) => {
 
         <div className="news__synopsis">{ news.synopsis }</div>
         {/* <div>Submitted by ${news.user.firstName} ${news.user.lastName}</div> */}
+
+        <button onClick={
+                () => {
+                    deleteNews(NewsArray)
+                        .then(() => {
+                            props.history.push("/news")
+                        })
+                }
+            }>
+                Delete
+            </button>
     </section>
 )
   }
