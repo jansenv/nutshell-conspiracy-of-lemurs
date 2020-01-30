70b5e0ef562bd09648673494001f78c7d9a2dfa9
Spencer Truett <srtruett1993@gmail.com>
2020-01-30 14:02:12 -0600

    initial code for filtering by friends

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -6,31 +6,68 @@ import { NewsContext } from "./NewsProvider"
 
   export default ({history,  news }) => {
       const url = `https://www.${news.url}`
-      const { NewsArray, deleteNews, updateNews } = useContext(NewsContext)
+      const { deleteNews } = useContext(NewsContext)
+      const news = (news => {
+
+        const FriendChecker = (friends) => {
+          const foundFriends = friends.filter(relat => {
+            if (relat.activeUserId === parseInt(sessionStorage.getItem("activeUser"))) {
+              return relat
+            }
+          })
       
+          let shouldIRender = false
+          let createdByMe = false
+          
+          if (news.userId === parseInt(sessionStorage.getItem("activeUser"))) {
+            shouldIRender = true
+            createdByMe = true
+          }
+      
+          foundFriends.forEach(element => {
+            if (element.user.id === news.userId) {
+              shouldIRender = true
+            }
+          });
+      
+          if (shouldIRender && createdByMe) {
+            return 
+            <section className="news">
+            <h3 className="news__name">
+              <a href={url}>{ news.title }</a>
+            </h3>
+    
+            <div className="news__synopsis">{ news.synopsis }</div>
+            {/* <div>Submitted by ${news.user.firstName} ${news.user.lastName}</div> */}
+    
+    
+             <button onClick={() => {history.push(`/news/edit/${news.id}`)
+                 }}>Edit</button>
+    
+            <button onClick={
+                    () => {deleteNews(news)
+                            .then(() => { history.push("/news")})
+                    }}>Delete</button>
+        </section>
+          }
+      
+          if (shouldIRender) {
+            return 
+            <section className="news">
+            <h3 className="news__name">
+              <a href={url}>{ news.title }</a>
+            </h3>
+    
+            <div className="news__synopsis">{ news.synopsis }</div>
+            {/* <div>Submitted by ${news.user.firstName} ${news.user.lastName}</div> */}
+        </section>
+          }
+      
+        }
+      
+        return FriendChecker(useFriends())
+      
+      })
 
- return (
-    <section className="news">
-        <h3 className="news__name">
-          <a href={url}>{ news.title }</a>
-        </h3>
 
-        <div className="news__synopsis">{ news.synopsis }</div>
-        {/* <div>Submitted by ${news.user.firstName} ${news.user.lastName}</div> */}
-
-
-         <button onClick={() => {
-                history.push(`/news/edit/${news.id}`)
-             }}>Edit</button>
-
-        <button onClick={
-                () => {
-                    deleteNews(news)
-                        .then(() => {
-                            history.push("/news")
-                        })
-                }
-            }>Delete</button>
-    </section>
-)
   }
